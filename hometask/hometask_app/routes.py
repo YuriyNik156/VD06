f0aec6a6aabe254c8ce528ca40e771f5f2efffd6
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-05 15:52:35 +0300

    update questionaire.py and routes.py

--- hometask/hometask_app/routes.py
+++ hometask/hometask_app/routes.py
@@ -8,4 +8,11 @@ queries = []
 def index():
     if request.method == "POST":
         name = request.form.get("name")
-        
+        age = request.form.get("age")
+        city = request.form.get("city")
+        hobby = request.form.get("hobby")
+        dish = request.form.get("dish")
+        if name and age and city and hobby:
+            queries.append({"name" : name, "age" : age, "city" : city, "hobby" : hobby, "dish" : dish})
+            return redirect(url_for("index"))
+        return renser_template("questions.html", queries = queries)
---
+++ hometask/questionnaire.py
@@ -0,0 +1,4 @@
+from hometask_app import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
